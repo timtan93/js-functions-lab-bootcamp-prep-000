3ea6658d027e536ac23339b15ae8e8d00167cf1a
Timothy Tan <timtan93@gmail.com>
2018-11-24 19:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name){
   return "Happy holdays, ${name}!"
 }
 
-function happyHolidayTo(name){
-  return "Happy ${holday}, ${name}!"
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}"
 }
