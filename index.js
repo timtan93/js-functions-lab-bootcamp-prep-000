c2f61e5278cad79d1f0b2619a0bae46296e22544
Timothy Tan <timtan93@gmail.com>
2018-11-24 19:15:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
 
-// write your code below!	function happyHolidays() {
+// write your code below!	
+function happyHolidays() {
   return "Happy holidays!"
 }
  function happyHolidaysTo(name) {
